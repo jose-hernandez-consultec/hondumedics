aa2d4cad46aa24792c565ec7229b529968bc491c
jose-hernandez-consultec <joedhernandez95@hotmail.com>
2020-01-10 10:25:38 -0600

    testing

--- server/queries.js
+++ server/queries.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'josehernandez',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'Vibranium0',
+  user: 'lmuoitwffydlio',
+  host: 'ec2-174-129-32-215.compute-1.amazonaws.com',
+  database: 'd5do7g2h9ar8dm',
+  password: '91c4e02f1115da2e24c112fd51e6543c662d4e9c56d4a81622944c436e4c67f4',
   port: 5432,
 });
 
